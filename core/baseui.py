3abd6098fd83417e7b68d7fa45188a088f2419cc
长沙七叶 <gan0ling@126.com>
2023-10-01 19:56:41 +0800

    使用新方法处理事件

--- core/baseui.py
+++ core/baseui.py
@@ -1,4 +1,4 @@
-from circuits import Component, Event
+from circuits import Component, Event, Debugger
 from circuits.core.events import started
 import PySimpleGUI as sg
 import atexit
@@ -10,64 +10,24 @@ class UIEvent(Event):
     """UiEvent Event"""
 
 class BaseUIComponent(Component):
-    def __init__(self, title, layout):
+    def __init__(self, title, layout, debug=False):
         super().__init__()
         sg.theme("DarkAmber")
         self.title = title
         self.layout = layout
         self.window = None
         self.window = sg.Window(self.title, self.layout, resizable=True, use_ttk_buttons=True)
-    
-    def run(self, socket=None):
-        """
-        在原有Manager的run方法上，增加pySimpleGUI的事件循环
-        """
+        if debug:
+            self += Debugger()
 
-        atexit.register(self.stop)
-
-        if current_thread().name == "MainThread":
-            try:
-                set_signal_handler(SIGINT, self._signal_handler)
-                set_signal_handler(SIGTERM, self._signal_handler)
-            except ValueError:
-                # Ignore if we can't install signal handlers
-                pass
-
-        self._running = True
-        self.root._executing_thread = current_thread()
-
-        # Setup Communications Bridge
-
-        if socket is not None:
-            from circuits.core.bridge import Bridge
-            Bridge(socket, channel=socket.channel).register(self)
-
-        self.fire(started(self))
-
-        try:
-            while True:
-                # event,values = self.window.read(timeout=50)
-                event,values = self.window.read()
-                if event == sg.WIN_CLOSED or event == "Exit":
-                    break
-                if event != "__TIMEOUT__":
-                    self.fire(UIEvent(event, values))
-                # if not self.running or not len(self._queue):
-                    # break
-                self.tick(0.05)
-            self.window.close()
-            # Fading out, handle remaining work from stop event
-            for _ in range(3):
-                self.tick(0.1)
-        except Exception as exc:
-            stderr.write("Unhandled ERROR: {}\n".format(exc))
-            stderr.write(format_exc())
-        finally:
-            try:
-                self.tick(0.5)
-            except Exception:
-                pass
-
-        self.root._executing_thread = None
-        self.__thread = None
-        self.__process = None
+    def uiRun(self):
+        self.start(process=False, link=self)
+        while True:
+            event,values = self.window.read()
+            if event == sg.WIN_CLOSED or event == "Exit":
+                break
+            self.fire(UIEvent(event, values))
+        self.window.close()
+        self.stop()
+        self.join()
+        
--- core/serialui.py
+++ core/serialui.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
 from baseui import BaseUIComponent
 import serial.tools.list_ports
+from circuits.io.serial import Serial, _open
+from circuits.io.events import close
 
 class SerialUI(BaseUIComponent):
     def __init__(self):
@@ -19,12 +21,33 @@ class SerialUI(BaseUIComponent):
             [sg.Multiline(size=(100, 50), key="-RECVTEXT-", expand_x=True, expand_y=True)],
             [sg.I(key="-SENDTEXT-"),sg.B("Send", key="-SEND-")]
         ]
-        super().__init__("Serial", self.layout)
+        super().__init__("Serial", self.layout, debug=True)
+        self._ser = None
         
     def UIEvent(self, *arg):
         event = arg[0]
         values = arg[1]
+        if event == "-OPENCLOSE-":
+            if self.window["-OPENCLOSE-"].GetText() == "Open":
+                #判断串口是否存在
+                if not values['-PORTLIST-']:
+                    return
+                #打开串口
+                if self._ser is None:
+                    #第一次打开
+                    self._ser = Serial(values["-PORTLIST-"], int(values["-BAUDRATELIST-"]))
+                    self += self._ser
+                else:
+                    #再次打开, 先发送关闭事件, 然后发送_open 事件
+                    self.fire(close(), self._ser.channel)
+                    self.fire(_open(port=values["-PORTLIST-"], baudrate=int(values["-BAUDRATELIST-"])), self._ser.channel)
+                self.window["-OPENCLOSE-"].update(text="Close")
+                self.window.refresh()
+            else:
+                self.fire(close(), self._ser.channel)
+                self.window["-OPENCLOSE-"].update(text="Open")
+                self.window.refresh()
 
 if __name__ == "__main__":
     serialUI = SerialUI()
-    serialUI.run()
+    serialUI.uiRun()
